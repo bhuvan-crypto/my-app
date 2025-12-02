603d302c508a3b01d218062bed2f2360cc0fdb1f
bhuvan-crypto <55475746+bhuvan-crypto@users.noreply.github.com>
2025-12-02 10:35:46 +0000

    updates

--- app/config.ts
+++ app/config.ts
@@ -1,3 +1,3 @@
-const API_BASE_URL = "https://laughing-dollop-w597r9gr9pj25vv4-3000.app.github.dev/"
 const MAIN_API_BASE_URL = "https://ecommerce-backend-nestjs.vercel.app/";
+const API_BASE_URL = MAIN_API_BASE_URL || "https://laughing-dollop-w597r9gr9pj25vv4-3000.app.github.dev/"
 export { API_BASE_URL };
